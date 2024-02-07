1300f940fc04c708e7ac51a5bf42ca34e5d745f5
Андрей Мещеринов <androidmesch@mail.ru>
2024-02-07 18:45:36 +0300

    .

--- init/js/load.js
+++ init/js/load.js
@@ -1,32 +1,31 @@
 function getData(onSuccess, onError) {
-  fetch('https://25.javascript.pages.academy/keksobooking/data')
+  fetch("https://25.javascript.htmlacademy.pro/keksobooking/data")
     .then((response) => {
-      if (response.ok){
+      if (response.ok) {
         return response.json();
       }
 
       throw new Error(`${response.status} ${response.statusText}`);
     })
     .then((data) => onSuccess(data))
-    .catch(() => onError('Не получилось загрузить похожие объявления'));
+    .catch(() => onError("Не получилось загрузить похожие объявления"));
 }
 
 function sendData(onSuccess, onError, body) {
-  fetch('https://25.javascript.pages.academy/keksobooking',
-    {
-      method: 'POST',
-      body
-    })
+  fetch("https://25.javascript.htmlacademy.pro/keksobooking", {
+    method: "POST",
+    body,
+  })
     .then((response) => {
-      if(response.ok) {
+      if (response.ok) {
         onSuccess();
       } else {
-        onError('Не удалось отправить данные формы');
+        onError("Не удалось отправить данные формы");
       }
     })
     .catch(() => {
-      onError('Не удалось отправить данные формы. Попробуйте еще раз.');
+      onError("Не удалось отправить данные формы. Попробуйте еще раз.");
     });
 }
 
-export {getData, sendData};
+export { getData, sendData };
